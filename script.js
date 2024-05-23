da3f1e5a535360aaffa530301523d3c6dbe448c3
Belhassen <152809495+BelhassenB@users.noreply.github.com>
2024-05-24 00:32:41 +0200

    Add functions to check for wins and draws

--- script.js
+++ script.js
@@ -1,40 +1,69 @@
-function players(name, marker) {
-    return {name, marker}
+function players(name, marker,score = 0) {
+    return {name, marker, score}
 }
 const game = ( () => {
-    let board = Array(9).fill(null)
+    let board = Array(9).fill(null)    
     const player1 = players("Xerox", "X")
     const player2 = players("Oreo", "O")
     let currentPlayer = player1
 
-    const winConditions = [
-        [0,1,2],[3,4,5],[6,7,8] //Rows
-        [0,3,6],[1,4,7],[2,5,8] //Columns
-        [0,4,8],[2,4,6] //Diagonal        
-    ]    
-
-    const switchPlayer = () => {
-        game.currentPlayer = game.currentPlayer === player1? 
-        player2: 
-        player1
-    };
+    const getCurrentPlayer = () => currentPlayer
 
     const gameBoard = ( () => {
+
         const playTurn = (index) => {
-            board[index] = game.currentPlayer.marker
-            switchPlayer()
+          if (!board[index]) {
+            board[index] = getCurrentPlayer().marker 
+            console.log(`${getCurrentPlayer().name} placed an ${getCurrentPlayer().marker} on the cell ${index}`)
+            
+                if (checkWinner()) {
+                    console.log(`${getCurrentPlayer().name} won this roud!`);
+                    
+                    ++getCurrentPlayer().score
+                } else if (checkDraw()) {
+                    console.log("This round was a tie")
+                } else {
+                    switchPlayer();
+                }
+            } else {
+            console.log("ERROR this cell is used");
+            }
+    
         }
+
+        const switchPlayer = () => {
+            currentPlayer = currentPlayer === player1? 
+            player2: 
+            player1
+        };
+
         const checkWinner = () => {
-            
-            
+            const winConditions = [
+                [0,1,2],[3,4,5],[6,7,8], //Rows
+                [0,3,6],[1,4,7],[2,5,8], //Columns
+                [0,4,8],[2,4,6] //Diagonal        
+            ]    
+
+            return winConditions.some(condition => 
+                condition.every(index => board[index] === getCurrentPlayer().marker)
+            )
+        }; 
+        
+        const checkDraw = () => {
+            return !checkWinner() && board.every(item => item !== null)
+                
         }
-        return {playTurn, checkWinner}
+        
+        const resetBoard = () => board = Array(9).fill(null)
+                
+        
+        return {playTurn, checkWinner, checkDraw}
 
 
     })()  
 
     return {board,
-        currentPlayer,
+        getCurrentPlayer,
         gameBoard,
     }
 })()
